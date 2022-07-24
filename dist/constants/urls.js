ee2e67a10d396fde06728166def855c539ca2f70
klmbk <klmbkmr@gmail.com>
2022-07-24 19:51:14 +0600

    edit package.json

--- dist/constants/urls.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.urls = void 0;
-exports.urls = {
-    YOUTUBE: 'https://www.youtube.com/watch?v=',
-    YOUTUTBE_MOBILE: 'https://youtu.be/',
-    YOUTUBE_PICTURE: 'https://i.ytimg.com/vi/'
-};
