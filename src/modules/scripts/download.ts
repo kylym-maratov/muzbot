ad19f9b936017a965b102c36342df515a62d7d9e
klmbk <klmbkmr@gmail.com>
2022-07-28 00:11:00 +0600

    edit var names

--- src/modules/scripts/download.ts
+++ src/modules/scripts/download.ts
@@ -1,8 +1,9 @@
-import { exec } from "node:child_process"
 import { createWriteStream } from "fs"
 import { get } from "https"
+import { join } from 'path'
 import NodeID3 from 'node-id3'
-import ytdl, { getBasicInfo } from 'ytdl-core'
+import { getBasicInfo } from 'ytdl-core'
+import stream from 'youtube-audio-stream'
 import { Context } from "telegraf"
 import { Update } from "telegraf/typings/core/types/typegram"
 import { clearLocalFiles } from "./clear"
@@ -10,7 +11,6 @@ import { urls } from "../../constants/urls"
 import { saveNewSong } from "../database/scripts/save"
 import { SongTypes } from "../database/schemas/song/types"
 import { Song } from "../database/schemas/song"
-import stream from 'youtube-audio-stream'
 import { translateWord } from "./translate"
 
 const downloading: string[] = []
@@ -52,13 +52,14 @@ export const downloadAudioFromYoutube = async (ctx: Context<Update>, id: string,
         downloading.push(id)
 
         const { videoDetails } = await getBasicInfo(urls.YOUTUBE + id)
-        const filepath: string = `${translateWord(videoDetails.title)}-${id}.mp3`
+        const audioFilePath: string = join(__dirname, `../../files/${translateWord(videoDetails.title)}-${id}.mp3`)
+        const pictureFilePath: string = join(__dirname, `../../files/${id}.jpg`)
         const { message_id } = await ctx.reply(`id: ${id}\n\n${videoDetails.title}\n\nDownloading audio file from youtube please wait...`)
-        await Promise.all([downloadAudio(id, filepath), downloadPicture(id)])
-        const { tags } = await compilingAudioFile(id, videoDetails, filepath)
+        await Promise.all([downloadAudio(id, audioFilePath), downloadPicture(id, pictureFilePath)])
+        const tags = await compilingAudioFile(videoDetails, audioFilePath, pictureFilePath)
 
         ctx.replyWithChatAction('upload_voice')
-        const { audio } = await ctx.replyWithAudio({ source: filepath }, {
+        const { audio } = await ctx.replyWithAudio({ source: audioFilePath }, {
             performer: tags.artist,
             title: tags.title,
             thumb: { source: tags.APIC },
@@ -73,7 +74,7 @@ export const downloadAudioFromYoutube = async (ctx: Context<Update>, id: string,
         })
 
         ctx.deleteMessage(message_id)
-        clearLocalFiles([filepath, `${id}.jpg`])
+        clearLocalFiles([pictureFilePath, audioFilePath])
         const index = downloading.findIndex((item) => item === id)
         downloading.splice(index, 1)
 
@@ -88,19 +89,19 @@ export const downloadAudioFromYoutube = async (ctx: Context<Update>, id: string,
 }
 
 //Download audio file from youtube, user youtube-dl in exec command
-const downloadAudio = (id: string, filepath: string) => {
+const downloadAudio = (id: string, audioFilePath: string) => {
     return new Promise((res, rej) => {
         stream(urls.YOUTUBE + id)
-            .pipe(createWriteStream(filepath))
+            .pipe(createWriteStream(audioFilePath))
             .on('finish', () => res(null))
             .on('error', (err) => rej(err))
     })
 }
 
 //Http download youtube video thumb
-const downloadPicture = (id: string) => {
+const downloadPicture = (id: string, pictureFilePath: string) => {
     return new Promise((res, rej) => {
-        const file = createWriteStream(`${id}.jpg`)
+        const file = createWriteStream(pictureFilePath)
         get(`${urls.YOUTUBE_PICTURE + id}/hqdefault.jpg`, response => {
             response.pipe(file)
 
@@ -112,7 +113,7 @@ const downloadPicture = (id: string) => {
 }
 
 //Compiling video and picture, and edit audio tags
-const compilingAudioFile = (id: string, videoDetails: any, filepath: string): Promise<{ tags: TagsTypes }> => {
+const compilingAudioFile = (videoDetails: any, audioFilePath: string, pictureFilePath: string): Promise<TagsTypes> => {
     return new Promise((res, rej) => {
         const arrTitle: string[] = videoDetails.title.split('-')
 
@@ -120,14 +121,12 @@ const compilingAudioFile = (id: string, videoDetails: any, filepath: string): Pr
             title: arrTitle[1] ? arrTitle[1] : arrTitle[0],
             artist: arrTitle[1] ? arrTitle[0] : "Unknown artist",
             album: "Unknown album",
-            APIC: `${id}.jpg`,
+            APIC: pictureFilePath,
             TRCK: "27"
         }
 
-        NodeID3.update(tags, filepath, options)
+        NodeID3.update(tags, audioFilePath, options)
 
-        res({
-            tags
-        })
+        res(tags)
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import express, { Express } from 'express'
 import { Context, Telegraf } from "telegraf"
+import { exists, mkdir } from 'fs'
+import { join } from 'path'
 import { connectDb } from './modules/database/connect'
 require('dotenv').config()
 
 const port: number = Number(process.env.PORT) || 3000
 const token: string = process.env.TOKEN || ''
 const dbUrl: string = process.env.DB_URL || ''
+const filesPatph = join(__dirname, '/files')
 
 const bot: Telegraf<Context> = new Telegraf(token)
 const app: Express = express()
+
+exists(filesPatph, (bool) => {
+    if (!bool) {
+        mkdir(filesPatph, (err) => {
+            if (err) console.log(`Error create directory check server.ts file: ${err}`)
+        })
+    }
+})
 
 connectDb(dbUrl)
     .then(() => {
