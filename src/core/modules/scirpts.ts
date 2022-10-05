03aba41688125c6c45c9a747b594527c4bb9c25c
Kylym <kylym.maratovv@gmail.com>
2022-10-05 14:16:23 +0600

    edit reamde

--- src/core/modules/scirpts.ts
+++ src/core/modules/scirpts.ts
@@ -1,12 +1,12 @@
-import {exists, unlink} from 'fs'
+ import *  as fs from 'fs'
 
 const a: any = { "Ё": "YO", "Й": "I", "Ц": "TS", "У": "U", "К": "K", "Е": "E", "Н": "N", "Г": "G", "Ш": "SH", "Щ": "SCH", "З": "Z", "Х": "H", "Ъ": "'", "ё": "yo", "й": "i", "ц": "ts", "у": "u", "к": "k", "е": "e", "н": "n", "г": "g", "ш": "sh", "щ": "sch", "з": "z", "х": "h", "ъ": "'", "Ф": "F", "Ы": "I", "В": "V", "А": "a", "П": "P", "Р": "R", "О": "O", "Л": "L", "Д": "D", "Ж": "ZH", "Э": "E", "ф": "f", "ы": "i", "в": "v", "а": "a", "п": "p", "р": "r", "о": "o", "л": "l", "д": "d", "ж": "zh", "э": "e", "Я": "Ya", "Ч": "CH", "С": "S", "М": "M", "И": "I", "Т": "T", "Ь": "'", "Б": "B", "Ю": "YU", "я": "ya", "ч": "ch", "с": "s", "м": "m", "и": "i", "т": "t", "ь": "'", "б": "b", "ю": "yu" };
 
 export const clearDownloadedFiles = (files: string[]) => {
     for (let i = 0; files.length > i; i++) {
-        exists(files[i], (bool) => {
+        fs.exists(files[i], (bool) => {
             if (bool) {
-                unlink(files[i], () => {})
+                fs.unlink(files[i], () => {})
             }
         })
     }
@@ -19,3 +19,5 @@ export const convertWord = (word: String) => {
 
     return newWord.replace(/[^a-zа-яё\s]/gi, '').replace(/\s+/g, '+')
 }
+
+ 
